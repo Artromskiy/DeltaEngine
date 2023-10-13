427a84f7d1a003754b361f5b03a1bd9a533add38
Rumtery <artromskiy@gmail.com>
2023-10-13 21:34:27 +0300

    Main page cleanup

--- Source/DeltaEngineEditor/App.xaml.cs
+++ Source/DeltaEngineEditor/App.xaml.cs
@@ -8,5 +8,12 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Title = "Delta Engine";
+            return window;
+        }
     }
 }
